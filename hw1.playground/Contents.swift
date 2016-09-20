18d329e454064177fd5b932d9be6d285624c0c3c
Daniel Guan <dguan@Daniels-MacBook-Pro.local>
2016-09-19 23:46:58 -0700

    hw1

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -22,11 +22,11 @@ class Words {
 //: ### variables the same type? If not, why?
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: They aren't the same type - **!** variables are implicitly unwrapped optionals that are **nil** initially, but shouldn't be in the future. **?** variables are allowed to be **nil** and can be tested for.
 
 
 //: ## Q2: Variable Types and Function Types
-    func arePalindromes(_ words: [String]) -> Bool {
+    static func arePalindromes(_ words: [String]) -> Bool {
         let reversedWords = words.map() {String($0.characters.reversed())}
         let numElements = words.count
         
@@ -35,20 +35,21 @@ class Words {
                 return false
             }
         }
+        return true
     }
 //: ### Why does the compiler dislike the **for loop**? Fix it.
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: Prior to Swift 3, since **let** is an immutable variable, `0 ..< numElements` would be an invalid way to represent a range and thus this for-loop would have been correctly represented in C-style as `for var i = 0; i < numElements; i++`. However, in Swift 3, C-style for-loops are no longer supported so the above is correct. This function arePalindromes is of type Bool, but will have no return value if all `words[i] == reversedWords[i]`: intuitively, if the for-loop completes we should `return true`. Lastly, this function should be a static function since it is not called on an instance of the class.
 
 
 //: ## Q3: More Functions and Object Initialization
-    class func isAnagram() -> Bool {
-        var countLetters : [Character : Int] //Line X
-        var lenA = self.wordA.characters.count
-        var lenB = self.wordB.characters.count
+    func isAnagram() -> Bool {
+        var countLetters = [Character : Int]() //Line X
+        let lenA = self.wordA.characters.count
+        let lenB = self.wordB.characters.count
         
         if lenA != lenB {
             return false
@@ -75,13 +76,13 @@ class Words {
             }
         }
         
-        for (letter, count) in countLetters {
+        for (_, count) in countLetters {
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
 //: ### What is the problem with declaring **countLetters** as we do in **Line X**,
 //: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
@@ -89,7 +90,7 @@ class Words {
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: `countLetters : [Character : Int]` is an incorrect instantiation of an empty array. `return nil` should be `return true` as this function is **Bool** type. This function should not be a `class func`, since it is called on instances of the class and uses their instance variables. `lenA` and `lenB` should both be `let` variables since they're unchanged throughout the function. Finally in line 79, `(letter, count)` should be changed to `(_, count)` if `letter` is unused in the loop.
     
     
 }
